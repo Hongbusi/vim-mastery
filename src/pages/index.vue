576e9acd01af4f3f941280750fddf3833dcbaa1a
Hongbusi <coderhbs@gmail.com>
2022-08-26 11:27:11 +0800

    chore: udpate

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,19 +6,22 @@ import { toggleDark } from '~/composables'
 <template>
   <div class="flex border border-gray-200 dark:border-gray-800 shadow-lg">
     <div v-for="(colShortcuts, index) in allShortcuts" :key="index" class="flex-1 p-6">
-      <div v-if="index === 0" class="flex justify-between items-center py-4 text-xl">
-        <h2 class="font-600">
-          Vim keyboard shortcuts
-        </h2>
-        <div class="flex">
-          <button class="mx-2 !outline-none" title="切换深色模式" @click="toggleDark()">
-            <div i="carbon-sun dark:carbon-moon" />
-          </button>
-          <a class="mx-2" rel="noreferrer" href="https://github.com/Hongbusi/vim-keyboard-shortcuts" target="_blank" title="GitHub">
-            <div i-carbon-logo-github />
-          </a>
+      <template v-if="index === 0">
+        <div class="flex justify-between items-center py-4 text-xl">
+          <h2 class="font-600">
+            Vim keyboard shortcuts
+          </h2>
+          <div class="flex">
+            <button class="mx-2 !outline-none" title="切换深色模式" @click="toggleDark()">
+              <div i="carbon-sun dark:carbon-moon" />
+            </button>
+            <a class="mx-2" rel="noreferrer" href="https://github.com/Hongbusi/vim-keyboard-shortcuts" target="_blank" title="GitHub">
+              <div i-carbon-logo-github />
+            </a>
+          </div>
         </div>
-      </div>
+        <a href="https://learn.cuixueshe.com/" class="pb-1 text-sm border-b border-gray-200/80 dark:border-gray-800/50 hover:border-gray-200" target="_blank">整理于崔大的键盘侠课程。</a>
+      </template>
 
       <div v-for="shortcuts in colShortcuts" :key="shortcuts.title" class="mt-4">
         <div class="pb-2 mb-2 border-b-2 border-gray-200 dark:border-gray-800">
