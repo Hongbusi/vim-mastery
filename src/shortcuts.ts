8f68eb538eb6510d87f3a5d7e7096117c2f324fd
Hongbusi <coderhbs@gmail.com>
2022-11-03 23:56:03 +0800

    chore: update

--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -3,12 +3,33 @@ import type { ColModuleShortcuts } from '~/types'
 const shortcuts: ColModuleShortcuts[] = [
   [
     {
-      title: '移动光标',
+      title: '命令模式',
       shortcuts: [
-        { key: 'k', description: '上移光标' },
-        { key: 'j', description: '下移光标' },
-        { key: 'h', description: '左移光标' },
-        { key: 'l', description: '右移光标' },
+        { key: 'h', description: '光标向左移动一个字符' },
+        { key: 'j', description: '光标向下移动一个字符' },
+        { key: 'k', description: '光标向上移动一个字符' },
+        { key: 'l', description: '光标向右移动一个字符' },
+        { key: 'i', description: '进入输入模式，在光标前输入' },
+        { key: 'a', description: '进入输入模式，在光标后输入' }
+      ]
+    },
+    {
+      title: '输入模式',
+      shortcuts: [
+        { key: 'esc', description: '退出输入模式，进入命令模式' },
+        { key: 'ctrl + [', description: '退出输入模式，进入命令模式' }
+      ]
+    },
+
+    // ---
+
+    {
+      title: '命令模式',
+      shortcuts: [
+        { key: 'h', description: '光标向左移动一个字符' },
+        { key: 'j', description: '光标向下移动一个字符' },
+        { key: 'k', description: '光标向上移动一个字符' },
+        { key: 'l', description: '光标向右移动一个字符' },
         { key: '0', description: '移动到行首' },
         { key: '$', description: '移动到行尾' },
         { key: '^', description: '移动到本行第一个不是 blank 字符的位置' },
@@ -20,9 +41,9 @@ const shortcuts: ColModuleShortcuts[] = [
     {
       title: '插入模式',
       shortcuts: [
-        { key: 'i', description: '从光标前插入字符' },
+        { key: 'i', description: '进入 Insert 模式，在光标前输入' },
         { key: 'I', description: '从行首插入字符' },
-        { key: 'a', description: '从光标后插入字符' },
+        { key: 'a', description: '进入 Insert 模式，在光标后输入' },
         { key: 'A', description: '从行尾插入字符' },
         { key: 'o', description: '从行后插入字符' },
         { key: 'O', description: '从行前插入字符' },
