8495e058080a57231763943a5f1dfa523425b799
Hongbusi <coderhbs@gmail.com>
2022-11-04 22:49:45 +0800

    chore: update readme

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## TODO
 
 - [x] 1
-- [ ] 2
+- [x] 2
 - [ ] 3
 - [ ] 4
 - [ ] 5
@@ -22,45 +22,6 @@
 - [ ] 16
 - [ ] 17
 - [ ] 18
-
-## Mac Setting
-
-- 快速移动：
-  - 配置：[https://github.com/VSCodeVim/Vim#mac](https://github.com/VSCodeVim/Vim#mac)；
-  - 修改键盘设置：系统偏好设置 -> 键盘 -> 按键重复 & 重复前延迟都调到最大。
-
-## [Karabiner Elements](https://github.com/pqrs-org/Karabiner-Elements)
-
-- `ctrl` 和 `caps` 调换位置；
-- `Left ctrl + hjkl to arrow keys Vim`。
-
-## VSCode Setting
-
-``` json
-{
-  "vim.normalModeKeyBindings": [
-    {
-      "before": ["H"],
-      "after": ["^"]
-    },
-    {
-      "before": ["L"],
-      "after": ["g", "_"]
-    }
-  ],
-
-  "vim.operatorPendingModeKeyBindings": [
-    {
-      "before": ["H"],
-      "after": ["^"]
-    },
-    {
-      "before": ["L"],
-      "after": ["g", "_"]
-    }
-  ]
-}
-```
 
 ## License
 
--- src/shortcuts.ts
+++ src/shortcuts.ts
@@ -3,66 +3,44 @@ import type { ColModuleShortcuts } from '~/types'
 const shortcuts: ColModuleShortcuts[] = [
   [
     {
-      title: '命令模式',
-      shortcuts: [
-        { key: 'h', description: '光标向左移动一个字符' },
-        { key: 'j', description: '光标向下移动一个字符' },
-        { key: 'k', description: '光标向上移动一个字符' },
-        { key: 'l', description: '光标向右移动一个字符' },
-        { key: 'i', description: '进入输入模式，在光标前输入' },
-        { key: 'a', description: '进入输入模式，在光标后输入' }
-      ]
-    },
-    {
-      title: '输入模式',
-      shortcuts: [
-        { key: 'esc', description: '退出输入模式，进入命令模式' },
-        { key: 'ctrl + [', description: '退出输入模式，进入命令模式' }
-      ]
-    },
-
-    // ---
-
-    {
-      title: '命令模式',
+      title: '移动光标',
       shortcuts: [
         { key: 'h', description: '光标向左移动一个字符' },
         { key: 'j', description: '光标向下移动一个字符' },
         { key: 'k', description: '光标向上移动一个字符' },
         { key: 'l', description: '光标向右移动一个字符' },
         { key: '0', description: '移动到行首' },
+        { key: '^', description: '移动到本行第一个不是 blank 字符的位置（改建为 shift + h）' },
         { key: '$', description: '移动到行尾' },
-        { key: '^', description: '移动到本行第一个不是 blank 字符的位置' },
-        { key: 'g_', description: '移动到本行最后一个不是 blank 字符的位置' },
-        { key: 'H', description: '移动到本行第一个不是 blank 字符的位置', changeKey: true },
-        { key: 'L', description: '移动到本行最后一个不是 blank 字符的位置', changeKey: true }
+        { key: 'g_', description: '移动到本行最后一个不是 blank 字符的位置（改建为 shift + l）' }
       ]
     },
     {
-      title: '插入模式',
+      title: '进入插入模式',
       shortcuts: [
-        { key: 'i', description: '进入 Insert 模式，在光标前输入' },
-        { key: 'I', description: '从行首插入字符' },
-        { key: 'a', description: '进入 Insert 模式，在光标后输入' },
-        { key: 'A', description: '从行尾插入字符' },
-        { key: 'o', description: '从行后插入字符' },
-        { key: 'O', description: '从行前插入字符' },
-        { key: 'ea', description: '从当前单词末尾插入字符' },
-        { key: 'esc', description: '退出插入模式' },
-        { key: 'ctrl + [', description: '退出插入模式' }
+        { key: 'i', description: '在光标前插入' },
+        { key: 'a', description: '在光标后插入' },
+        { key: 'I', description: '在光标所在行的行首插入' },
+        { key: 'A', description: '在光标所在行的行尾插入' },
+        { key: 'o', description: '在光标所在行的上一行行首插入' },
+        { key: 'O', description: '在光标所在行的下一行行首插入' }
+      ]
+    },
+    {
+      title: '退出插入模式',
+      shortcuts: [
+        { key: 'esc', description: '退出插入模式，进入正常模式' },
+        { key: 'ctrl + [', description: '退出插入模式，进入正常模式' }
       ]
     }
   ],
   [
     {
-      title: '剪切和粘贴',
+      title: '复制、粘贴、剪切',
       shortcuts: [
-        { key: 'yy', description: '复制一行' },
-        { key: '2yy', description: '复制二行' },
-        { key: 'p', description: '在光标后粘贴' },
-        { key: 'P', description: '在光标前粘贴' },
-        { key: 'dd', description: '剪切一行' },
-        { key: '2dd', description: '剪切二行' }
+        { key: 'yy', description: '复制当前行' },
+        { key: 'p', description: '粘贴' },
+        { key: 'dd', description: '剪切当前行' }
       ]
     }
   ]
